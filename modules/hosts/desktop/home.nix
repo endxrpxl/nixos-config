df2dc33c8962bf4e51177b5b130879e4cd63613e
endxrpxl <ansgar.joerss@t-online.de>
2026-04-29 20:56:22 +0200

    bashrc bitwarden as ssh agent

--- modules/hosts/desktop/home.nix
+++ modules/hosts/desktop/home.nix
@@ -5,6 +5,13 @@
       self.homeModules.desktop
     ];
 
+    programs.bash = {
+      enable = true;
+      bashrcExtra = ''
+        export SSH_AUTH_SOCK=${self.homeDir}/.bitwarden-ssh-agent.sock
+      '';
+    };
+
     home.stateVersion = "25.11";
   };
 }
