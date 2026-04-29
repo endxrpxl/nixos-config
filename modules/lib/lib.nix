f8231db4f70145f9f2f9d10b4de72f922921e74a
endxrpxl <ansgar.joerss@t-online.de>
2026-04-29 20:56:32 +0200

    homeDir var

--- modules/lib/lib.nix
+++ modules/lib/lib.nix
@@ -1,7 +1,8 @@
 { self, lib, ... }: {
   flake = {
     username = lib.mkDefault "ansgar";
-    dotDir = "/home/${self.username}/nixos-config/.dotfiles";
+    homeDir = "/home/${self.username}";
+    dotDir = "${self.homeDir}/nixos-config/.dotfiles";
     dotConfig = "${self.dotDir}/.config";
     stateDir = "${self.dotDir}/.local/state";
   };
