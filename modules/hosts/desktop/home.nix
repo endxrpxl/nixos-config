d723347706e02b9298994c06ff5fabac18f6c6a4
endxrpxl <ansgar.joerss@t-online.de>
2026-07-13 09:59:31 +0200

    Add Emacs bin directory to PATH in bashrc

--- modules/hosts/desktop/home.nix
+++ modules/hosts/desktop/home.nix
@@ -9,6 +9,7 @@
       enable = true;
       bashrcExtra = ''
         export SSH_AUTH_SOCK=${self.homeDir}/.bitwarden-ssh-agent.sock
+        export PATH="$HOME/.config/emacs/bin:$PATH"
 
         ip-fix () {
             sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
